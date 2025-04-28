6d289b01e9ab1b8cd706171263f84128f6303338
Flozzzyy <flozzytiktok@gmail.com>
2025-04-28 06:43:03 +0300

    Its my 1st project on django

---
+++ testproject/main/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.mainpage, name='home'),
+    path('about/', views.about_page, name='about'),
+    path('contacts/', views.kontaks, name='kontakt')
+]
